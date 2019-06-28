fe713a21e084f3700a1cc5329a1a9a5d7a10cb83
Holiday Pettijohn <hpettijohn20@vcschools.org>
2019-06-27 21:18:59 -0700

    Fixed missing python file which broke app
    Also updated primitive physics code

--- Physics/physics.py
+++ Physics/physics.py
@@ -5,6 +5,7 @@ physics.py: Physics calculator for swerve drive
 
 import json
 from math import atan2, sqrt
+from time import time
 
 import pid
 
@@ -16,6 +17,8 @@ class Swerve:
     self.position = list(start_position)
     #Swerve Wheel Target
     self.swerve_target = 0
+    #Last update time - used for velocity motion calculations
+    self.last_time = time()
     #Swerve Wheel PID
     self.swerve_pid = pid.PID(params["swerve_p"], params["swerve_i"], params["swerve_d"])
     #TODO: Velocity PID
@@ -52,11 +55,18 @@ class Swerve:
     """
     Calculates the effects of physics given the motor velocites and time passed
     """
+    t = time()
+    delta_time = t-self.last_time
+    self.last_time = t
     wheel_power = params["voltage"]*params["max_current"]
     frswerve_motor_velocity = (wheel_power*self.motor_velocities["frswerve"])/params["robot_weight"]
     brswerve_motor_velocity = (wheel_power*self.motor_velocities["brswerve"])/params["robot_weight"]
     flswerve_motor_velocity = (wheel_power*self.motor_velocities["flswerve"])/params["robot_weight"]
     blswerve_motor_velocity = (wheel_power*self.motor_velocities["blswerve"])/params["robot_weight"]
+    self.encoders["frswerve"] += frswerve_motor_velocity*delta_time
+    self.encoders["brswerve"] += brswerve_motor_velocity*delta_time
+    self.encoders["flswerve"] += flswerve_motor_velocity*delta_time
+    self.encoders["blswerve"] += blswerve_motor_velocity*delta_time
 
   def sendControls(self, x, y, z):
     """
@@ -70,25 +80,24 @@ class Swerve:
     self.swerveWheelsTo(direction)
   
   def arcadeDrive(self, x, y):
-    self.motor_velocities["frwheel"] = x+y
-    self.motor_velocities["brwheel"] = x+y
-    self.motor_velocities["flwheel"] = x-y
-    self.motor_velocities["blwheel"] = x-y
+    right = x+y
+    left = x-y
+    self.motor_velocities["frwheel"] = right
+    self.motor_velocities["brwheel"] = right
+    self.motor_velocities["flwheel"] = left
+    self.motor_velocities["blwheel"] = left
 
   def swerveWheelsTo(self, direction):
     """
     Uses a pid loop to swerve wheels to a certain position
     """
+    motors = ["frswerve", "brswerve", "flswerve", "blswerve"]
     velocity = self.swerve_pid.update(error = self.swerve_target-self.encoders["frswerve"])
-    self.motor_velocities["frswerve"] = velocity
-    self.motor_velocities["brswerve"] = velocity
-    self.motor_velocities["flswerve"] = velocity
-    self.motor_velocities["blswerve"] = velocity
+    for motor in motors:
+      self.motor_velocities[motor] = velocity
 
 def loadParams():
   f = open("params.json")
   params = json.load(f)
   f.close()
   return params
-
-
---
+++ Physics/primitivePhysics.py
@@ -0,0 +1,129 @@
+"""
+primitivePhysics.py: Simple version of swerve drive physics engine
+6/27/2019 Holiday Pettijohn
+"""
+
+import json
+from math import atan2, sqrt, sin, cos
+from time import time
+
+class Swerve:
+  def __init__(self, start_position = (0, 0, 0)):
+    #Position is x, y, direction
+    self.position = list(start_position)
+    #Swerve Wheel Target
+    self.swerve_target = 0
+    #Last update time - used for velocity motion calculations
+    self.last_time = time()
+    #Wheel encoders work in Panda grid units, which are assumed to be like meters
+    #Swerve encoders work in radians
+    self.encoders = {"frwheel": 0,
+                     "brwheel": 0,
+                     "flwheel": 0,
+                     "blwheel": 0,
+                     "frswerve": 0,
+                     "brswerve": 0,
+                     "flswerve": 0,
+                     "brswerve": 0}
+    #Vectors are stored as 2D [magnitude, radian] pairs
+    self.vectors = {"frwheel": [0, 0],
+                    "brwheel": [0, 0],
+                    "flwheel": [0, 0],
+                    "blwheel": [0, 0],
+                    "frswerve": [0, 0],
+                    "brswerve": [0, 0],
+                    "flswerve": [0, 0],
+                    "brswerve": [0, 0],
+                    "frame": [0, 0]}
+    self.motor_velocities = {"frwheel": 0,
+                             "brwheel": 0,
+                             "flwheel": 0,
+                             "blwheel": 0,
+                             "frswerve": 0,
+                             "brswerve": 0,
+                             "flswerve": 0,
+                             "brswerve": 0}
+
+  def update(self):
+    """
+    Calculates the effects of physics given the motor velocites and time passed
+    """
+    #TODO: Implement Friction
+    t = time()
+    delta_time = t-self.last_time
+    self.last_time = t
+    self.updateVectors()
+    self.updatePositions()
+
+  def updateVectors(self):
+    """
+    Updates the vectors output by the motors
+    """
+    #Velocity (m/s) = amps*volts (watts) /robot_weight (newtons)
+    #= newton*meter/second*newton = meter/second
+    #TODO: Add equasions for swerve wheels
+    #TODO: This equasion does not go through rpm, but simplifies the
+    #motor's mechanics into an efficency power loss
+    self.vectors["frwheel"] = [(params["voltage"]*params["max_current"]*self.motor_velocities["frwheel"])
+                               *params["motor_efficency"]/params["robot_weight"],
+                               self.encoders["frswerve"]]
+    self.vectors["brwheel"] = [(params["voltage"]*params["max_current"]*self.motor_velocities["brwheel"])
+                               *params["motor_efficency"]/params["robot_weight"],
+                               self.encoders["brswerve"]]
+    self.vectors["flwheel"] = [(params["voltage"]*params["max_current"]*self.motor_velocities["flwheel"])
+                               *params["motor_efficency"]/params["robot_weight"],
+                               self.encoders["flswerve"]]
+    self.vectors["blwheel"] = [(params["voltage"]*params["max_current"]*self.motor_velocities["blwheel"])
+                               *params["motor_efficency"]/params["robot_weight"],
+                               self.encoders["blswerve"]]
+
+  def updatePositions(self):
+    """
+    Updates the wheel and robot positions based on vector data
+    """
+    #TODO: Account for each wheel rotating individually as opposed to
+    #treating their forces as coming from robot's the center of gravity
+    #Calculate robot position change
+    wheels = ["frwheel", "brwheel", "flwheel", "blwheel"]
+    x = 0
+    y = 0
+    for wheel in wheels:
+      x += cos(self.vectors[wheel][0])*self.vectors[wheel][1]
+      y += sin(self.vectors[wheel][0])*self.vectors[wheel][1]
+    self.position[0] += x
+    self.position[1] += y
+
+  def sendControls(self, x, y, z):
+    """
+    Sets motor velocites based on the given controls
+    """
+    magnitude = sqrt(x**2+y**2)
+    direction = atan2(y, x)
+    twist = z
+    self.arcadeDrive(twist, magnitude)
+    self.swerve_target = direction
+    self.swerveWheels(direction)
+  
+  def arcadeDrive(self, x, y):
+    self.motor_velocities["frwheel"] = x+y
+    self.motor_velocities["brwheel"] = x+y
+    self.motor_velocities["flwheel"] = x-y
+    self.motor_velocities["blwheel"] = x-y
+
+  def swerveWheels(self, direction):
+    """
+    Swerves wheels to a given direction
+    """
+    #TODO: This does not yet simulate the physics of turning the wheels
+    self.motor_velocities["frswerve"] = direction
+    self.motor_velocities["brswerve"] = direction
+    self.motor_velocities["flswerve"] = direction
+    self.motor_velocities["blswerve"] = direction
+
+def loadParams():
+  f = open("params.json")
+  params = json.load(f)
+  f.close()
+  return params
+
+params = loadParams()
